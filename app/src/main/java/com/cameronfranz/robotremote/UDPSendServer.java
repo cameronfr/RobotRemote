a52f4431f39956cc86e47f5fa366ed8a256a84b7
cameronfr <catechboy@hotmail.com>
2016-12-19 00:49:48 +0900

    renamed UDPCommandServer to UDPSendServer, improved error handling and update rate

---
+++ app/src/main/java/com/cameronfranz/robotremote/UDPSendServer.java
@@ -0,0 +1,80 @@
+package com.cameronfranz.robotremote;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+import android.content.Context;
+
+
+/**
+ * Created by cameronfranz on 7/13/16.
+ */
+public class UDPSendServer implements Runnable {
+
+    private Map<String,Integer> ctrl_state_outgoing;
+    private InetAddress destinationIP;
+    private int destinationPort;
+    private int updateRate;
+
+
+    public UDPSendServer(Map<String,Integer> outgoingMap) {
+        ctrl_state_outgoing = outgoingMap;
+    }
+
+    public void setDestination(String ip,int port) throws UnknownHostException {
+        destinationIP = InetAddress.getByName(ip);
+        destinationPort = port;
+    }
+
+    public void setUpdateRate (int rate) {
+        updateRate = rate;
+    }
+
+    public void run() {
+        DatagramSocket sendSocket;
+        Log.d("CommandServer","Starting UDP send server with IP destination: " + destinationIP +
+                " and port: " + Integer.toString(destinationPort));
+        try {
+            sendSocket = new DatagramSocket();
+            while(!Thread.currentThread().isInterrupted()) {
+                try {
+                    String stringOut = ctrl_state_outgoing.toString().replace("=", ":");
+                    sendCommandPacket(sendSocket, stringOut);
+                }
+                catch (IOException e) {
+                    Log.d("CommandServer","Error sending packet");
+                    e.printStackTrace();
+                }
+                try {
+                    Thread.sleep(1000 / updateRate);
+                }
+                catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            Log.d("CommandServer","Shutting down UDP send server...");
+            sendSocket.close();
+        }
+        catch (Exception e) {
+            Log.d("CommandServer","Error binding send socket");
+            e.printStackTrace();
+        }
+    }
+
+    private void sendCommandPacket (DatagramSocket socket, String message) throws IOException{
+        byte[] sendData = message.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,destinationIP,destinationPort);
+        socket.send(sendPacket);
+        //Log.d("CommandThread","Sent message: " + stringOut);
+    }
+
+
+
+}
