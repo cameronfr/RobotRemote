78486760e12d89787cad83d75eb489ce4ce3f228
cameronfr <catechboy@hotmail.com>
2016-12-19 00:40:52 +0900

    added more prefs, separated send and recieve threads, dynamic telemetry size.

--- app/src/main/java/com/cameronfranz/robotremote/ControllerActivity.java
+++ app/src/main/java/com/cameronfranz/robotremote/ControllerActivity.java
@@ -1,12 +1,21 @@
 package com.cameronfranz.robotremote;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.net.UnknownHostException;
 import java.util.HashMap;
@@ -15,39 +24,59 @@ import java.util.Map;
 public class ControllerActivity extends AppCompatActivity {
 
     //output to UDP and input from UDP respectively
-    public Map<String,Integer> ctrl_state_outgoing;
-    public Map<String,String> ctrl_state_incoming;
+    private Map<String,Integer> ctrlStateOutgoing;
     public static int TOUCHPAD_RANGE = 100;
-    public UDPCommandServer commandServer;
-    public Thread commandThread;
+    private UDPSendServer sendServer;
+    private Thread sendThread;
+    private UDPReceiveServer receiveServer;
+    private Thread receiveThread;
+    TextView telemetryText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ctrl_state_outgoing = new HashMap<String,Integer>();
-        initializeDefaultState();
+        ctrlStateOutgoing = new HashMap<String,Integer>();
         setContentView(R.layout.activity_controller);
-        FrameLayout ctrl_leftTouchPad = (FrameLayout) findViewById(R.id.ctrl_leftTouchPad);
-        FrameLayout ctrl_rightTouchPad = (FrameLayout) findViewById(R.id.ctrl_rightTouchPad);
+        initializeDefaultState();
 
         SharedPreferences spref = PreferenceManager.getDefaultSharedPreferences(this);
         String controller_ip = spref.getString("controller_ip","192.168.1.1");
-        int controller_port = spref.getInt("controller_port",12345);
+        int sendPort = spref.getInt("controller_send_port",8111);
+        int recievePort = spref.getInt("controller_recieve_port",8112);
+        int updateRate = spref.getInt("controller_update_rate",60);
+
 
         try {
-            commandServer = new UDPCommandServer(ctrl_state_outgoing, ctrl_state_incoming);
-            commandServer.setDestination(controller_ip, controller_port);
+            sendServer = new UDPSendServer(ctrlStateOutgoing);
+            sendServer.setDestination(controller_ip, sendPort);
+            sendServer.setUpdateRate(updateRate);
         }
         catch (UnknownHostException e) {
-            e.printStackTrace();
+            Context context = getApplicationContext();
+            CharSequence text = "Error: Unknown Host!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
+        receiveServer = new UDPReceiveServer(new UDPReceiveServer.RecievedMessageListener() {
+            @Override
+            public void onRecieveMessage(String message) {
+                updateTelemetryText(message);
+            }
+        });
+        receiveServer.setReceivePort(recievePort);
 
-        commandThread = new Thread(commandServer);
-        commandThread.start();
+        sendThread = new Thread(sendServer);
+        receiveThread = new Thread(receiveServer);
+        sendThread.start();
+        receiveThread.start();
 
+        telemetryText = (TextView) findViewById(R.id.telemetryText);
+        FrameLayout ctrl_leftTouchPad = (FrameLayout) findViewById(R.id.ctrl_leftTouchPad);
+        FrameLayout ctrl_rightTouchPad = (FrameLayout) findViewById(R.id.ctrl_rightTouchPad);
         ctrl_leftTouchPad.setOnTouchListener(touchPadHandler);
         ctrl_rightTouchPad.setOnTouchListener(touchPadHandler);
-
         findViewById(R.id.ctrl_btn1).setOnTouchListener(buttonsHandler);
         findViewById(R.id.ctrl_btn2).setOnTouchListener(buttonsHandler);
         findViewById(R.id.ctrl_btn3).setOnTouchListener(buttonsHandler);
@@ -73,12 +102,12 @@ public class ControllerActivity extends AppCompatActivity {
 
             switch (view.getId()) {
                 case R.id.ctrl_leftTouchPad:
-                    ctrl_state_outgoing.put("'leftTouchPadX'",normalizedXPos);
-                    ctrl_state_outgoing.put("'leftTouchPadY'",normalizedYPos);
+                    ctrlStateOutgoing.put("'leftTouchPadX'",normalizedXPos);
+                    ctrlStateOutgoing.put("'leftTouchPadY'",normalizedYPos);
                     break;
                 case R.id.ctrl_rightTouchPad:
-                    ctrl_state_outgoing.put("'rightTouchPadX'",normalizedXPos);
-                    ctrl_state_outgoing.put("'rightTouchPadY'",normalizedYPos);
+                    ctrlStateOutgoing.put("'rightTouchPadX'",normalizedXPos);
+                    ctrlStateOutgoing.put("'rightTouchPadY'",normalizedYPos);
                     break;
 
             }
@@ -100,22 +129,22 @@ public class ControllerActivity extends AppCompatActivity {
 
             switch (view.getId()) {
                 case R.id.ctrl_btn1:
-                    ctrl_state_outgoing.put("'btn1'",btnVal);
+                    ctrlStateOutgoing.put("'btn1'",btnVal);
                     break;
                 case R.id.ctrl_btn2:
-                    ctrl_state_outgoing.put("'btn2'",btnVal);
+                    ctrlStateOutgoing.put("'btn2'",btnVal);
                     break;
                 case R.id.ctrl_btn3:
-                    ctrl_state_outgoing.put("'btn3'",btnVal);
+                    ctrlStateOutgoing.put("'btn3'",btnVal);
                     break;
                 case R.id.ctrl_btn4:
-                    ctrl_state_outgoing.put("'btn4'",btnVal);
+                    ctrlStateOutgoing.put("'btn4'",btnVal);
                     break;
                 case R.id.ctrl_btn5:
-                    ctrl_state_outgoing.put("'btn5'",btnVal);
+                    ctrlStateOutgoing.put("'btn5'",btnVal);
                     break;
                 case R.id.ctrl_btn6:
-                    ctrl_state_outgoing.put("'btn6'",btnVal);
+                    ctrlStateOutgoing.put("'btn6'",btnVal);
                     break;
             }
 
@@ -128,42 +157,61 @@ public class ControllerActivity extends AppCompatActivity {
 
 
     public void initializeDefaultState() {
-        ctrl_state_outgoing.put("'leftTouchPadX'",0);
-        ctrl_state_outgoing.put("'leftTouchPadY'",0);
-        ctrl_state_outgoing.put("'rightTouchPadX'",0);
-        ctrl_state_outgoing.put("'rightTouchPadY'",0);
-        ctrl_state_outgoing.put("'btn1'",0);
-        ctrl_state_outgoing.put("'btn2'",0);
-        ctrl_state_outgoing.put("'btn3'",0);
-        ctrl_state_outgoing.put("'btn4'",0);
-        ctrl_state_outgoing.put("'btn5'",0);
-        ctrl_state_outgoing.put("'btn6'",0);
+        ctrlStateOutgoing.put("'leftTouchPadX'",0);
+        ctrlStateOutgoing.put("'leftTouchPadY'",0);
+        ctrlStateOutgoing.put("'rightTouchPadX'",0);
+        ctrlStateOutgoing.put("'rightTouchPadY'",0);
+        ctrlStateOutgoing.put("'btn1'",0);
+        ctrlStateOutgoing.put("'btn2'",0);
+        ctrlStateOutgoing.put("'btn3'",0);
+        ctrlStateOutgoing.put("'btn4'",0);
+        ctrlStateOutgoing.put("'btn5'",0);
+        ctrlStateOutgoing.put("'btn6'",0);
 
     }
-
-    public void updateTelemetry() {}
 
     public void updateTouchPadMarkers() {
         View lPad = findViewById(R.id.ctrl_leftTouchPad);
         View lMarker = findViewById(R.id.leftTouchPadMarker);
-        lMarker.setX(lPad.getWidth() * ctrl_state_outgoing.get("'leftTouchPadX'")
+        lMarker.setX(lPad.getWidth() * ctrlStateOutgoing.get("'leftTouchPadX'")
                 /(TOUCHPAD_RANGE*2) + lPad.getWidth()/2 - lMarker.getWidth()/2);
-        lMarker.setY(lPad.getHeight() * -ctrl_state_outgoing.get("'leftTouchPadY'")
+        lMarker.setY(lPad.getHeight() * -ctrlStateOutgoing.get("'leftTouchPadY'")
                 /(TOUCHPAD_RANGE*2) + lPad.getHeight()/2 - lMarker.getHeight()/2);
 
         View rPad = findViewById(R.id.ctrl_rightTouchPad);
         View rMarker = findViewById(R.id.rightTouchPadMarker);
-        rMarker.setX(ctrl_state_outgoing.get("'rightTouchPadX'")*rPad.getWidth()
+        rMarker.setX(ctrlStateOutgoing.get("'rightTouchPadX'")*rPad.getWidth()
                 /(TOUCHPAD_RANGE*2) + rPad.getWidth()/2 - rMarker.getWidth()/2);
-        rMarker.setY(-ctrl_state_outgoing.get("'rightTouchPadY'")*rPad.getHeight()
+        rMarker.setY(-ctrlStateOutgoing.get("'rightTouchPadY'")*rPad.getHeight()
                 /(TOUCHPAD_RANGE*2) + rPad.getHeight()/2 - rMarker.getHeight()/2);
 
 
     }
 
-    public void onStop() {
-        super.onStop();
-        commandThread.interrupt();
+    public void updateTelemetryText (final String s) {
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+
+        Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                //Log.d("CommandServer","View height: " + ((View)telemetryText.getParent()).getHeight());
+                //Log.d("CommandServer","Text Height:" + Integer.toString(telemetryText.getHeight()));
+                telemetryText.setText(s);
+
+                if (telemetryText.getHeight() == ((View)telemetryText.getParent()).getHeight()) {
+                    //Log.d("CommandServer",telemetryText.getTextSize() + " " + Integer.toString(telemetryText.getHeight()));
+                    telemetryText.setTextSize(TypedValue.COMPLEX_UNIT_PX,telemetryText.getTextSize()-1);
+                }
+            }
+        };
+        mainHandler.post(myRunnable);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        sendThread.interrupt();
+        receiveThread.interrupt();
     }
 
     public int limitRange(int n, int min, int max) {
@@ -173,3 +221,4 @@ public class ControllerActivity extends AppCompatActivity {
     }
 
 }
+
